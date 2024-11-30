4717cde4836598ff0be6ef9809efc154c99b113a
Wanderhappen <miracle12301230@gmail.com>
2024-12-01 03:07:26 +0400

    fix: test4

--- src/shared/config/base-query.ts
+++ src/shared/config/base-query.ts
@@ -1,16 +1,15 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query'
-import type { RootState } from './store'
 
 export const createBaseQuery = (baseUrl: string) =>
   fetchBaseQuery({
     baseUrl,
     credentials: 'include',
-    prepareHeaders: (headers, { getState }) => {
-      const token = (getState() as RootState).auth.accessToken
-
-      if (token) {
-        headers.set('authorization', `Bearer ${token}`)
-      }
+    prepareHeaders: headers => {
+      // const token = (getState() as RootState).auth.accessToken
+      //
+      // if (token) {
+      //   headers.set('authorization', `Bearer ${token}`)
+      // }
 
       return headers
     },
