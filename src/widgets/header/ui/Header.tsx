1c699e411435b95cd07ee7b72ee5b51bbea58227
Matvei <motya2222@gmail.com>
2024-12-11 13:43:20 +0300

    fix: remove single-use HeaderContainer component

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { LanguageSwitcher } from '@/features/languageSwitcher'
 import s from './Header.module.scss'
-import { HeaderContainer } from '@/shared/ui/HeaderContainer'
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 import { clsx } from 'clsx'
 import { Logo } from './logo'
@@ -10,10 +9,10 @@ type Props = ComponentPropsWithoutRef<'header'>
 
 export const Header = forwardRef<ElementRef<'header'>, Props>(({ className, ...rest }, ref) => (
   <header className={clsx(s.header, className)} ref={ref} {...rest}>
-    <HeaderContainer className={s.headerContainer}>
+    <div className={s.headerContainer}>
       <Logo />
       <LanguageSwitcher />
-    </HeaderContainer>
+    </div>
   </header>
 ))
 
