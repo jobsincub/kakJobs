bd9252f510a7c8eab14e0314ada05ab6d6017fd7
Wanderhappen <miracle12301230@gmail.com>
2024-12-18 23:04:30 +0400

    refactor: use i18n in schemes

--- src/shared/lib/validations/emailSchema.ts
+++ src/shared/lib/validations/emailSchema.ts
@@ -1,5 +1,20 @@
+import { useTranslation } from '@/shared/config'
 import { z } from 'zod'
 
-export const emailSchema = z.object({
-  email: z.string().email('ВЫ ВВЕЛИ НЕ ПОЧТУ').default(''),
-})
+export const useEmailSchema = () => {
+  const {
+    t: {
+      shared: {
+        validations: { emailSchema: schema },
+      },
+    },
+  } = useTranslation()
+
+  const emailSchema = z.object({
+    email: z.string().email(schema.email).default(''),
+  })
+
+  return {
+    emailSchema,
+  }
+}
