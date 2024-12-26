d6c3cdd7622e2f2bd89e01fa318a9acca332ad9c
Ayan <nazbievayan16@gmail.com>
2024-12-26 15:37:22 +0500

    corrected route

--- src/shared/router/routes.ts
+++ src/shared/router/routes.ts
@@ -2,7 +2,7 @@ export const routes = {
   home: '/',
   signIn: '/auth/signin',
   legal: {
-    termsOfService: '/legal/terms-of-serivce',
-    privacyPolicy: '/legal/privacy-policy',
+    termsOfService: '/terms-of-serivce',
+    privacyPolicy: '/privacy-policy',
   },
 }
