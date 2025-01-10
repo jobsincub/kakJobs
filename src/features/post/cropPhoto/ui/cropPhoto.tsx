70307d13f8db0a867da079fca5611781ec13ada7
Ayan <nazbievayan16@gmail.com>
2025-01-10 17:16:06 +0500

    new task - cropping starting layout

--- src/features/post/cropPhoto/ui/cropPhoto.tsx
+++ src/features/post/cropPhoto/ui/cropPhoto.tsx
@@ -1,10 +1,21 @@
 import { selectPhotos } from '@/widgets/createPost'
-import { DialogBody, DialogHeader, DialogTitle } from '@wandrehappen/ui-kit'
+import {
+  DialogBody,
+  DialogHeader,
+  DialogTitle,
+  Expand,
+  ImageOutline,
+  MaximizeOutline,
+} from '@wandrehappen/ui-kit'
 import Image from 'next/image'
 import React from 'react'
 import { useSelector } from 'react-redux'
+import s from './cropPhoto.module.scss'
 
 export const CropPhoto = () => {
+  // const [crop, setCrop] = useState<Crop>({
+  //   // aspect: 1,
+  // })
   const photos = useSelector(selectPhotos)
 
   return (
@@ -13,7 +24,28 @@ export const CropPhoto = () => {
         <DialogTitle>Cropping</DialogTitle>
       </DialogHeader>
       <DialogBody>
-        <Image src={URL.createObjectURL(photos[0].file)} alt={'1'} width={300} height={300}></Image>
+        {/*<ReactCrop crop={crop} onChange={c => setCrop(c)}>*/}
+        <div style={{ maxWidth: '100%', position: 'relative' }}>
+          <Image
+            src={URL.createObjectURL(photos[0].file)}
+            alt={'1'}
+            width={100}
+            height={100}
+            style={{ width: '100%', height: 'auto' }}
+          />
+          <div className={s.iconContainer}>
+            <div className={s.iconWrapper}>
+              <Expand />
+            </div>
+            <div className={s.iconWrapper}>
+              <MaximizeOutline />
+            </div>
+            <div className={s.iconWrapper}>
+              <ImageOutline />
+            </div>
+          </div>
+        </div>
+        {/*</ReactCrop>*/}
       </DialogBody>
     </>
   )
