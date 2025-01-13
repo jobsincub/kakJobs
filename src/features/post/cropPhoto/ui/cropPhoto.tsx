951e170d86fd42ae4cdc679e38e2e94bffbef7b4
Ayan <nazbievayan16@gmail.com>
2025-01-13 16:08:10 +0500

    commit before downloading react easy crop

--- src/features/post/cropPhoto/ui/cropPhoto.tsx
+++ src/features/post/cropPhoto/ui/cropPhoto.tsx
@@ -8,28 +8,31 @@ import {
   MaximizeFill,
   MaximizeOutline,
 } from '@wandrehappen/ui-kit'
-import Image from 'next/image'
 import React, { ChangeEvent, useState } from 'react'
 import { useSelector } from 'react-redux'
 import s from './cropPhoto.module.scss'
-import ReactCrop, { Crop } from 'react-image-crop'
+import ReactCrop, { Crop, PixelCrop } from 'react-image-crop'
 
 export const CropPhoto = () => {
   const photos = useSelector(selectPhotos)
-  const [active, setActive] = useState(false)
+  const [zoomActive, setZoomActive] = useState(false)
+  const [isOpenCrop, setIsOpenCrop] = useState(false)
   const [scale, setScale] = useState(1)
   const [crop, setCrop] = useState<Crop>()
+  const [aspect, setAspect] = useState<number | undefined>(16 / 9)
+  const [completedCrop, setCompletedCrop] = useState<PixelCrop>()
 
-  const onClickHandler = () => {
-    setActive(true)
+  const onZoomClickHandler = () => {
+    setZoomActive(!zoomActive)
+  }
+
+  const onCropClickHandler = () => {
+    setIsOpenCrop(!isOpenCrop)
   }
 
   const handleScale = (e: ChangeEvent<HTMLInputElement>) => {
     setScale(+e.currentTarget.value)
   }
-
-  console.log(photos)
-
   return (
     <>
       <DialogHeader>
@@ -38,27 +41,47 @@ export const CropPhoto = () => {
       <DialogBody>
         {/*<Image src={photos[0].file} alt={'1'} width={300} height={300}></Image>*/}
         <div style={{ maxWidth: '100%', position: 'relative' }}>
-          <ReactCrop crop={crop} onChange={(c, percentageCrop) => setCrop(percentageCrop)}>
-            <Image
-              src={photos[0].file}
-              alt={'1'}
-              width={100}
-              height={100}
-              style={{ width: '100%', height: 'auto', transform: `scale(${scale})` }}
-            />
+          <ReactCrop crop={crop} onChange={c => setCrop(c)} onComplete={c => setCompletedCrop(c)}>
+            {/*<Image*/}
+            {/*  src={photos[0].file}*/}
+            {/*  alt={'1'}*/}
+            {/*  width={100}*/}
+            {/*  height={100}*/}
+            {/*  style={{ width: '100%', height: 'auto', transform: `scale(${scale})` }}*/}
+            {/*  onLoad={onImageLoad}*/}
+            {/*/>*/}
+            <img src={photos[0].file} alt={'Crop me'} />
           </ReactCrop>
           <div className={s.iconContainer}>
             <div style={{ display: 'flex', gap: '15px' }}>
-              <div className={s.iconWrapper}>
-                <Expand />
+              <div className={s.iconWrapper} onClick={onCropClickHandler}>
+                {isOpenCrop && (
+                  <div className={s.cropContainer}>
+                    <ul className={s.list}>
+                      <li>
+                        <span>Original</span> <ImageOutline />
+                      </li>
+                      <li>
+                        <span>1:1</span>
+                      </li>
+                      <li>
+                        <span>4:5</span>
+                      </li>
+                      <li>
+                        <span>16:9</span>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+                {isOpenCrop ? <Expand /> : <Expand fill={'#fffff'} />}
               </div>
-              <div className={s.iconWrapper} onClick={onClickHandler}>
-                {active && (
+              <div className={s.iconWrapper} onClick={onZoomClickHandler}>
+                {zoomActive && (
                   <div className={s.zoomContainer}>
                     <input type={'range'} value={scale} onChange={e => handleScale(e)} />
                   </div>
                 )}
-                {active ? <MaximizeFill /> : <MaximizeOutline />}
+                {zoomActive ? <MaximizeFill /> : <MaximizeOutline />}
               </div>
             </div>
             <div className={s.iconWrapper}>
