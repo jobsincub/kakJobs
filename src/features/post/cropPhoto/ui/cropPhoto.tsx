4fbe64916dbfd11b741aebf444519c32f75dac50
Ayan <nazbievayan16@gmail.com>
2025-01-13 14:42:47 +0500

    commit before downloading react easy crop

--- src/features/post/cropPhoto/ui/cropPhoto.tsx
+++ src/features/post/cropPhoto/ui/cropPhoto.tsx
@@ -5,15 +5,30 @@ import {
   DialogTitle,
   Expand,
   ImageOutline,
+  MaximizeFill,
   MaximizeOutline,
 } from '@wandrehappen/ui-kit'
 import Image from 'next/image'
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import { useSelector } from 'react-redux'
 import s from './cropPhoto.module.scss'
+import ReactCrop, { Crop } from 'react-image-crop'
 
 export const CropPhoto = () => {
   const photos = useSelector(selectPhotos)
+  const [active, setActive] = useState(false)
+  const [scale, setScale] = useState(1)
+  const [crop, setCrop] = useState<Crop>()
+
+  const onClickHandler = () => {
+    setActive(true)
+  }
+
+  const handleScale = (e: ChangeEvent<HTMLInputElement>) => {
+    setScale(+e.currentTarget.value)
+  }
+
+  console.log(photos)
 
   return (
     <>
@@ -21,23 +36,29 @@ export const CropPhoto = () => {
         <DialogTitle>Cropping</DialogTitle>
       </DialogHeader>
       <DialogBody>
-        <Image src={photos[0].file} alt={'1'} width={300} height={300}></Image>
-        {/*<ReactCrop crop={crop} onChange={c => setCrop(c)}>*/}
+        {/*<Image src={photos[0].file} alt={'1'} width={300} height={300}></Image>*/}
         <div style={{ maxWidth: '100%', position: 'relative' }}>
-          <Image
-            src={photos[0].file}
-            alt={'1'}
-            width={100}
-            height={100}
-            style={{ width: '100%', height: 'auto' }}
-          />
+          <ReactCrop crop={crop} onChange={(c, percentageCrop) => setCrop(percentageCrop)}>
+            <Image
+              src={photos[0].file}
+              alt={'1'}
+              width={100}
+              height={100}
+              style={{ width: '100%', height: 'auto', transform: `scale(${scale})` }}
+            />
+          </ReactCrop>
           <div className={s.iconContainer}>
             <div style={{ display: 'flex', gap: '15px' }}>
               <div className={s.iconWrapper}>
                 <Expand />
               </div>
-              <div className={s.iconWrapper}>
-                <MaximizeOutline />
+              <div className={s.iconWrapper} onClick={onClickHandler}>
+                {active && (
+                  <div className={s.zoomContainer}>
+                    <input type={'range'} value={scale} onChange={e => handleScale(e)} />
+                  </div>
+                )}
+                {active ? <MaximizeFill /> : <MaximizeOutline />}
               </div>
             </div>
             <div className={s.iconWrapper}>
@@ -45,7 +66,6 @@ export const CropPhoto = () => {
             </div>
           </div>
         </div>
-        {/*</ReactCrop>*/}
       </DialogBody>
     </>
   )
