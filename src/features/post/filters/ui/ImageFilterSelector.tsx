be94175ac40b6fe87c5706db49c71a210a8e5d11
Wanderhappen <miracle12301230@gmail.com>
2025-01-16 01:02:39 +0400

    refactor

---
+++ src/features/post/filters/ui/ImageFilterSelector.tsx
@@ -0,0 +1,78 @@
+import Image from 'next/image'
+import React, { useMemo, useRef } from 'react'
+import s from './ImageFilterSelector.module.scss'
+
+type Filter = {
+  name: string
+  style: string
+}
+
+type Props = {
+  image: string
+  onImageSelect: (filteredImage: string) => void
+  customFilters?: Filter[]
+}
+
+const defaultFilters: Filter[] = [
+  { name: 'Normal', style: 'none' },
+  { name: 'Clarendon', style: 'contrast(125%) brightness(110%)' },
+  { name: 'Gingham', style: 'grayscale(50%) brightness(105%)' },
+  { name: 'Moon', style: 'grayscale(100%) contrast(120%)' },
+  { name: 'Lark', style: 'brightness(130%) saturate(110%)' },
+  { name: 'Reyes', style: 'sepia(20%) brightness(120%) contrast(90%)' },
+  { name: 'Juno', style: 'contrast(120%) saturate(150%)' },
+  { name: 'Aden', style: 'sepia(30%) brightness(120%) saturate(85%)' },
+  { name: 'Perpetua', style: 'brightness(120%) saturate(120%)' },
+]
+
+export const ImageFilterSelector: React.FC<Props> = ({ image, onImageSelect, customFilters }) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  const filters = useMemo(() => customFilters || defaultFilters, [customFilters])
+
+  const applyFilterHandler = (filterStyle: string) => {
+    const canvas = canvasRef.current
+    if (!canvas || !image) return
+
+    const context = canvas.getContext('2d')
+    if (!context) return
+
+    const img = document.createElement('img')
+    img.src = image
+
+    img.onload = () => {
+      canvas.width = img.width
+      canvas.height = img.height
+
+      context.filter = filterStyle
+      context.drawImage(img, 0, 0)
+
+      const dataURL = canvas.toDataURL()
+      onImageSelect(dataURL)
+    }
+  }
+
+  return (
+    <div className={s.selector}>
+      <canvas ref={canvasRef} style={{ display: 'none' }} />
+
+      {filters.map(filter => (
+        <div
+          key={filter.name}
+          className={s.imageWrapper}
+          onClick={() => applyFilterHandler(filter.style)}
+        >
+          <Image
+            src={image}
+            className={s.image}
+            alt={filter.name}
+            width={108}
+            height={108}
+            style={{ filter: filter.style }}
+          />
+          <span className={s.filterName}>{filter.name}</span>
+        </div>
+      ))}
+    </div>
+  )
+}
--- src/features/post/filters/ui/filters.tsx
+++ src/features/post/filters/ui/filters.tsx
@@ -9,15 +9,21 @@ import {
   DialogTitle,
 } from '@wandrehappen/ui-kit'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
+import s from './filters.module.scss'
+import { ImageFilterSelector } from './ImageFilterSelector'
 
 export const Filters = () => {
   const photos = useSelector(selectPhotos)
   const currentImage = photos[0].imageUrl
   console.log(currentImage)
+
+  const [selectedImage, setSelectedImage] = useState<string>(currentImage)
+  console.log(selectedImage)
+
   return (
-    <DialogContent>
+    <DialogContent className={s.content}>
       <DialogHeader isCloseIconVisible={false}>
         <Button variant={'link'}>
           <ArrowIos color={'white'} />
@@ -25,17 +31,23 @@ export const Filters = () => {
         <DialogTitle>Filters</DialogTitle>
         <Button variant={'link'}>Next</Button>
       </DialogHeader>
-      <DialogBody>
+      <DialogBody className={s.body}>
         <DialogDescription style={{ display: 'none' }}>
           This dialog allows you to enhance your photo by applying various filters. Experiment with
           different styles to achieve the desired look before.
         </DialogDescription>
         <Image
-          src={currentImage}
+          src={selectedImage}
           alt={'1'}
-          width={500} // Задаём ширину
-          height={300} // Задаём высоту
+          width={490} // Задаём ширину
+          height={504} // Задаём высоту
+          style={{
+            // aspectRatio: '1 / 1',
+            objectFit: 'cover',
+          }}
+          className={s.test}
         />
+        <ImageFilterSelector image={currentImage} onImageSelect={setSelectedImage} />
       </DialogBody>
     </DialogContent>
   )
