b7edb4029a3393fec8a2bfda71fdb7bd63f1d583
Wanderhappen <miracle12301230@gmail.com>
2025-01-17 02:30:17 +0400

    refactor: add callback

--- src/features/post/ui/createPostHeader.tsx
+++ src/features/post/ui/createPostHeader.tsx
@@ -6,9 +6,10 @@ import { HTMLAttributes } from 'react'
 type Props = {
   title: string
   nextButtonText: string
+  nextButtonHandler: () => void
 } & HTMLAttributes<HTMLDivElement>
 
-export const CreatePostHeader = ({ title, nextButtonText }: Props) => {
+export const CreatePostHeader = ({ title, nextButtonText, nextButtonHandler }: Props) => {
   const dispatch = useAppDispatch()
 
   const previousStepHandler = () => {
@@ -25,7 +26,7 @@ export const CreatePostHeader = ({ title, nextButtonText }: Props) => {
         <ArrowIos color={'white'} />
       </Button>
       <DialogTitle>{title}</DialogTitle>
-      <Button onClick={nextStepHandler} variant={'link'}>
+      <Button onClick={nextButtonHandler ?? nextStepHandler} variant={'link'}>
         {nextButtonText}
       </Button>
     </DialogHeader>
