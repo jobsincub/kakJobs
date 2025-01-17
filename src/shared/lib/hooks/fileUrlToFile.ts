350afe50e361fd1af2ace9a715d0feffcd540133
Matvei <motya2222@gmail.com>
2025-01-17 14:16:30 +0300

    refactor: rename blobUrl to fileUrl

--- src/shared/lib/hooks/fileUrlToFile.ts
+++ src/shared/lib/hooks/fileUrlToFile.ts
@@ -1,10 +1,10 @@
-export const blobUrlToFile = async (
-  blobUrl: string,
+export const fileUrlToFile = async (
+  fileUrl: string,
   fileName: string = 'photo.jpg'
 ): Promise<File> => {
-  const response = await fetch(blobUrl)
+  const response = await fetch(fileUrl)
   if (!response.ok) {
-    throw new Error(`Failed to fetch blob: ${response.statusText}`)
+    throw new Error(`Failed to fetch file: ${response.statusText}`)
   }
   const blob = await response.blob()
   return new File([blob], fileName, { type: blob.type })
