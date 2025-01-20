88c1d9af361c90550413f2bef5d8e2a286ec0663
TanyaMalina <105852395+TanyaMalina@users.noreply.github.com>
2025-01-20 18:34:35 +0300

    refactor: add routing folder for profili page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+'use client'
+
+import { useSelector } from 'react-redux'
+import { useRouter } from 'next/navigation'
+import { Button } from '@wandrehappen/ui-kit'
+import { selectUserId } from '@/entities/user/model/authSlice'
+
 export default function Home() {
-  return <div>Home</div>
+  const userId = useSelector(selectUserId)
+  const router = useRouter()
+  const handler = () => {
+    router.push(`/profile/${userId}`)
+  }
+
+  return <Button onClick={handler}>My profile</Button>
 }
