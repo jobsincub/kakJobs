9c336f091ab2f184615706231c2b4e62422f8312
Matvei <motya2222@gmail.com>
2025-01-20 15:59:25 +0300

    fix: change createPost mutation
    Add photo file conversion before sending to the server using queryFn.

--- src/entities/post/api/postApi.ts
+++ src/entities/post/api/postApi.ts
@@ -1,16 +1,53 @@
 import { baseQueryWithReauth } from '@/shared/api'
 import { createApi } from '@reduxjs/toolkit/query/react'
+import { convertUrlToFile } from '@/shared/lib/hooks'
+import { FetchBaseQueryError } from '@reduxjs/toolkit/query'
+import { PhotoFile } from '../model/postSlice'
 
 export const postApi = createApi({
   reducerPath: 'postApi',
   baseQuery: baseQueryWithReauth,
   endpoints: builder => ({
-    createPost: builder.mutation<ApiResponse<PostData>, { description: string; photos: string[] }>({
-      query: body => ({
-        body,
-        url: 'posts',
-        method: 'POST',
-      }),
+    createPost: builder.mutation<
+      ApiResponse<PostData>,
+      { description?: string; photos: PhotoFile[] }
+    >({
+      queryFn: async ({ description, photos }, _api, _extraOptions, baseQuery) => {
+        try {
+          const formData = new FormData()
+
+          if (description) {
+            formData.append('description', description)
+          }
+
+          await Promise.all(
+            photos.map(async photo => {
+              const file = await convertUrlToFile({
+                fileUrl: photo.url,
+                fileName: photo.name,
+              })
+              formData.append('photos', file)
+            })
+          )
+
+          const result = await baseQuery({
+            url: 'posts',
+            method: 'POST',
+            body: formData,
+          })
+
+          return result.data
+            ? { data: result.data as ApiResponse<PostData> }
+            : { error: result.error as FetchBaseQueryError }
+        } catch (error) {
+          return {
+            error: {
+              status: 'CUSTOM_ERROR',
+              error: (error as Error).message,
+            },
+          }
+        }
+      },
     }),
   }),
 })
