e0185672c2312dcbcef44aa46f7420a57f9071e9
Matvei <motya2222@gmail.com>
2025-01-20 16:44:45 +0300

    refactor: move PublishDialogContent logic to custom hook

--- src/features/post/publish/lib/usePublishDialogContent.ts
+++ src/features/post/publish/lib/usePublishDialogContent.ts
@@ -1,10 +1,28 @@
 import { SubmitHandler } from 'react-hook-form'
 import { PublishPostFormValues } from '@/features/post/publish'
 import { useAppDispatch } from '@/shared/lib'
+import { reset, selectPhotos, useCreatePostMutation } from '@/entities/post'
+import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { ROUTES } from '@/shared/router/routes'
 
 export const usePublishDialogContent = () => {
+  const router = useRouter()
   const dispatch = useAppDispatch()
-  const onSubmit: SubmitHandler<PublishPostFormValues> = data => console.log(data)
+  const photos = useSelector(selectPhotos)
+  const [createPost, { isSuccess }] = useCreatePostMutation()
+
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(reset())
+      router.push(ROUTES.HOME)
+    }
+  }, [isSuccess, dispatch, router])
+
+  const onSubmit: SubmitHandler<PublishPostFormValues> = data => {
+    createPost({ ...data, photos })
+  }
 
   return { dispatch, onSubmit }
 }
