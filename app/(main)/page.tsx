13ee3653819cfb6414e8964091ed493addfc8658
TanyaMalina <105852395+TanyaMalina@users.noreply.github.com>
2025-03-06 16:39:19 +0300

    refactor: change router pages

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,22 +1,3 @@
-'use client'
-
-import { useSelector } from 'react-redux'
-import { useRouter } from 'next/navigation'
-import { Button } from '@wandrehappen/ui-kit'
-import { selectUserId } from '@/entities/user/model/authSlice'
-import { CreatePost } from '@/widgets/createPost'
-
 export default function Home() {
-  const userId = useSelector(selectUserId)
-  const router = useRouter()
-  const handler = () => {
-    router.push(`/profile/${userId}`)
-  }
-
-  return (
-    <>
-      <Button onClick={handler}>My profile</Button>
-      <CreatePost />
-    </>
-  )
+  return <div>Home</div>
 }
