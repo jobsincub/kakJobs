7f10de7447deb0a2f782a4c76c720f8c59d95c05
Wanderhappen <miracle12301230@gmail.com>
2025-03-09 16:10:11 +0400

    refactor

--- src/pages/profile/lib/useMyProfilePage.ts
+++ src/pages/profile/lib/useMyProfilePage.ts
@@ -1,22 +1,21 @@
+import { useGetUsersPostsQuery } from '@/entities/post'
+import { selectUserId } from '@/entities/user/model/authSlice'
+import { useRouter } from 'next/navigation'
 import { useEffect, useRef, useState } from 'react'
 
 import { useSelector } from 'react-redux'
-import { selectUserId } from '@/entities/user/model/authSlice'
-import { useGetUsersPostsQuery } from '@/entities/post'
-import { useRouter } from 'next/navigation'
 
 export const useMyProfilePage = () => {
   const userId = useSelector(selectUserId)
+
   const router = useRouter()
+
   if (!userId) {
     router.push('/')
   }
   const [page, setPage] = useState(1)
-  const { data, error, isLoading, isFetching } = useGetUsersPostsQuery(
-    { userId, page },
-    { skip: !userId }
-  )
-  console.log(data)
+
+  const { data, isFetching } = useGetUsersPostsQuery({ userId, page }, { skip: !userId })
 
   const posts = data?.items || []
   const totalPages = data?.meta.totalPages || 1
