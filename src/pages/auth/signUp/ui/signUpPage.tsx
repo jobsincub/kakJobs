8984468f0ffaa53970484701eeef1757141e0a58
Matvei <motya2222@gmail.com>
2025-07-17 17:39:27 +0300

    feat: add GoogleOAuthButton to SignUpPage

--- src/pages/auth/signUp/ui/signUpPage.tsx
+++ src/pages/auth/signUp/ui/signUpPage.tsx
@@ -10,7 +10,7 @@ import Link from 'next/link'
 import React from 'react'
 import { UseSignUpPage } from '../lib/useSignUpPage'
 import s from './signUpPage.module.scss'
-import { OAuthButtons } from '@/features/oauth'
+import { GoogleOAuthButton } from '@/features/oauth'
 
 export const SignUpPage = () => {
   const { page, customError, isSuccess, email, onSubmit } = UseSignUpPage()
@@ -18,10 +18,12 @@ export const SignUpPage = () => {
   return (
     <Page mt={24} as={AuthFormWrapper}>
       <EmailSentDialog email={email} isOpen={isSuccess} />
-      <Typography asChild color={'light-100'} variant={'h1'}>
+      <Typography asChild color={'light-100'} variant={'h1'} className={s.title}>
         <h1>{page.title}</h1>
       </Typography>
-      <OAuthButtons />
+      <div className={s.oAuthButtons}>
+        <GoogleOAuthButton />
+      </div>
       <SignUpForm error={customError} onSubmit={onSubmit} />
       <div className={s.textWrapper}>
         <Typography asChild color={'light-100'} variant={'regular16'} className={s.text}>
