66413bceaf35c3342469c2e52a0ff4ea5e91cfdc
Matvei <motya2222@gmail.com>
2025-07-24 16:31:10 +0300

    refactor: move legal route group to auth route segment

--- src/shared/router/routes.ts
+++ src/shared/router/routes.ts
@@ -1,10 +1,6 @@
 export const ROUTES = {
   HOME: '/',
   CREATE_POST: '/createPost',
-  LEGAL: {
-    TERMS_OF_SERVICE: '/terms-of-service',
-    PRIVACY_POLICY: '/privacy-policy',
-  },
   AUTH: {
     SIGN_UP: '/auth/sign-up',
     SIGN_IN: '/auth/sign-in',
@@ -12,6 +8,10 @@ export const ROUTES = {
     EMAIL_CONFIRM: '/auth/email-confirm',
     FORGOT_PASSWORD: '/auth/forgot-password',
     RESEND_VERIFICATION: '/auth/resend-verification',
+    LEGAL: {
+      TERMS_OF_SERVICE: '/auth/terms-of-service',
+      PRIVACY_POLICY: '/auth/privacy-policy',
+    },
   },
   PROFILE: (id: number) => `/profile/${id}` as const,
 }
