ded0aaabbeefbdb41cf8d7f1ee68223d9776f49b
Matvei <motya2222@gmail.com>
2025-07-31 16:02:10 +0300

    refactor: add body padding-left dynamically

--- src/widgets/sidebar/lib/useSidebar.tsx
+++ src/widgets/sidebar/lib/useSidebar.tsx
@@ -2,6 +2,7 @@ import { selectUserId } from '@/entities/user/model/authSlice'
 import { useTranslation } from '@/shared/config'
 import { useAppSelector } from '@/shared/lib/store/redux'
 import { selectIsLoggedIn } from '@/entities/user/model/authSlice'
+import { useEffect } from 'react'
 
 export const useSidebar = () => {
   const {
@@ -12,6 +13,10 @@ export const useSidebar = () => {
 
   const myId = useAppSelector(selectUserId)
   const isLoggedIn = useAppSelector(selectIsLoggedIn)
+
+  useEffect(() => {
+    document.body.style.paddingLeft = isLoggedIn ? '220px' : '0'
+  }, [isLoggedIn])
 
   return { sidebar, myId, isLoggedIn }
 }
